fdee074f9c54e09447fdbcce50e81959dd3c560e
rrliang <lollypopking445@gmail.com>
2022-02-03 11:57:06 -0500

    Added basic register screen

--- src/emergencynexus/LoginScreenController.java
+++ src/emergencynexus/LoginScreenController.java
@@ -1,11 +1,19 @@
 package emergencynexus;
 
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class LoginScreenController {
 
@@ -24,7 +32,18 @@ public class LoginScreenController {
     }
 
     @FXML
-    void registerClicked(MouseEvent event) {
-        System.out.println("register!");
+    void registerClicked(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("registerScreen.fxml"));
+        /* 
+         * if "fx:controller" is not set in fxml
+         * fxmlLoader.setController(NewWindowController);
+         */
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("New Window");
+        stage.setScene(scene);
+        stage.show();
+        ((Node)(event.getSource())).getScene().getWindow().hide();
     }
 }
---
+++ src/emergencynexus/RegisterScreenController.java
@@ -0,0 +1,21 @@
+package emergencynexus;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RegisterScreenController {
+
+    @FXML
+    private TextField emailAddress;
+
+    @FXML
+    private Button registerEmailContinueButton;
+
+    @FXML
+    void continueEmail(ActionEvent event) {
+
+    }
+
+}
