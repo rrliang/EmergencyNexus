329116a9c6d119e9d8d73615ca83f2d4b3eb6752
rrliang <42505429+rrliang@users.noreply.github.com>
2022-04-07 15:50:23 -0400

    Delete src/main/java/com/flyinggeese/emergencynexus/src/main directory

--- src/main/java/com/flyinggeese/emergencynexus/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.flyinggeese.emergencynexus {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.flyinggeese.emergencynexus to javafx.fxml;
-    exports com.flyinggeese.emergencynexus;
-}
