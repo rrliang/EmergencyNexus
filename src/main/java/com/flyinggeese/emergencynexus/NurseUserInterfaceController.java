3a0e5b54342154a91e73d7556de67e40cf72cda9
Rahul Kumar <83566650+kumarrah2002@users.noreply.github.com>
2022-04-11 21:13:08 -0400

    Add files via upload

--- src/main/java/com/flyinggeese/emergencynexus/NurseUserInterfaceController.java
+++ src/main/java/com/flyinggeese/emergencynexus/NurseUserInterfaceController.java
@@ -2,14 +2,20 @@ package com.flyinggeese.emergencynexus;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.control.ChoiceBox;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class NurseUserInterfaceController implements Initializable {
@@ -117,24 +123,96 @@ public class NurseUserInterfaceController implements Initializable {
     private Color x4;
 
     @FXML
-    private TextField firstName, lastName;
-    @FXML
-    private ChoiceBox<String> feeling, symptoms;
+    private TextField fullName, physician, icant, bp, room, saline, notes;
 
+    @FXML
+    private ChoiceBox<String> feeling, symptoms, medication, injection;
+
+    @FXML
+    private DatePicker visit, checkin, checkout;
+
+    @FXML
+    private Button submit;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         //PATIENT VISIT FORM CHOICE BOXES
+        ObservableList<String> hurt = FXCollections.observableArrayList();
+        hurt.addAll("Head", "Shoulders", "Chest", "Arms", "Hips", "Legs", "Knees", "Feet");
+        symptoms.setItems(hurt);
+        symptoms.setValue("Head");
 
         ObservableList<String> s = FXCollections.observableArrayList();
         s.addAll("Good", "Okay", "Bad", "Terrible");
         feeling.setItems(s);
         feeling.setValue("Okay");
 
-        ObservableList<String> hurt = FXCollections.observableArrayList();
-        hurt.addAll("Head", "Shoulders", "Chest", "Arms", "Hips", "Legs", "Knees", "Feet");
-        symptoms.setItems(hurt);
+        ObservableList<String> meds = FXCollections.observableArrayList();
+        meds.addAll("None", "Tylenol", "Aspirin", "Insulin");
+        medication.setItems(meds);
+        medication.setValue("None");
+
+        ObservableList<String> inj = FXCollections.observableArrayList();
+        inj.addAll("None", "COVID-19 Vaccine", "Flu Shot");
+        injection.setItems(inj);
+        injection.setValue("None");
+    }
+
+    @FXML
+    void submitData(ActionEvent event) throws IOException, SQLException {
+        ConnectToDatabase db = new ConnectToDatabase();
+        db.makeJDBCConnection();
+        String query = "INSERT INTO emergency_nexus.visit (dateofvisit, symptomsmyhurt, symptomsifeel, symptomsicant, bloodpressure, admissionstatuscheckin, admissionstatuscheckout, admissionstatusroom, amountofsaline, givenmedication, injectionsgiven, notesandobservations, patient, primaryphysician) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement pst = db.getConnection().prepareStatement(query);
+
+        pst.setString(1, ((TextField)visit.getEditor()).getText());
+        pst.setString(2, symptoms.getSelectionModel().getSelectedItem().toString());
+        pst.setString(3, feeling.getSelectionModel().getSelectedItem().toString());
+        pst.setString(4, icant.getText());
+        pst.setString(5, bp.getText());
+        pst.setString(6, ((TextField)checkin.getEditor()).getText());
+        pst.setString(7, ((TextField)checkout.getEditor()).getText());
+        pst.setString(8, room.getText());
+        pst.setString(9, saline.getText());
+        pst.setString(10, medication.getSelectionModel().getSelectedItem().toString());
+        pst.setString(11, injection.getSelectionModel().getSelectedItem().toString());
+        pst.setString(12, notes.getText());
+        pst.setString(13, fullName.getText());
+        pst.setString(14, physician.getText());
+
+        pst.execute();
+        pst.close();
+        clearFields();
+    }
+    public void clearFields() {
+        fullName.clear();
+        physician.clear();
+        visit.setValue(null);
         symptoms.setValue("Head");
+        feeling.setValue("Okay");
+        icant.clear();
+        bp.clear();
+        checkin.setValue(null);
+        checkout.setValue(null);
+        room.clear();
+        saline.clear();
+        medication.setValue("None");
+        injection.setValue("None");
+        notes.clear();
+    }
+    @FXML
+    private TextField namelookup;
+    @FXML
+    private DatePicker dob;
+
+    void lookUpPatient(ActionEvent event) throws IOException, SQLException{
+        ConnectToDatabase db = new ConnectToDatabase();
+        db.makeJDBCConnection();
+        String searchQuery = "SELECT * FROM emergency_nexus.patients WHERE fullname = ? AND dateofbirth = ?";
+        PreparedStatement pst = db.getConnection().prepareStatement(searchQuery);
+
+        pst.setString(1, namelookup.getText());
+        pst.setString(2, ((TextField)dob.getEditor()).getText());
 
 
     }
