b6261d7cd2f30cf973f0bd74fcd76a7502981bed
rrliang <42505429+rrliang@users.noreply.github.com>
2022-04-13 10:04:26 -0400

    Update nurse user interface

--- src/main/java/com/flyinggeese/emergencynexus/NurseUserInterfaceController.java
+++ src/main/java/com/flyinggeese/emergencynexus/NurseUserInterfaceController.java
@@ -1,17 +1,11 @@
 package com.flyinggeese.emergencynexus;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -30,18 +24,27 @@ public class NurseUserInterfaceController implements Initializable {
     //FXML VARIABLES FOR REGISTER FORM
     @FXML
     private TextField registerAddressText, registerAllergiesText, registerEmailText, registerEthnicityText,
-            registerGuardianNameText, registerGuardianPhoneText, registerHeightText, registerInsuranceChoice,
-            registerMedicationText, registerNameText, registerPhoneText, registerPreConditionsText, registerPronounsText,
-            registerReligionText, registerWeightText;
+            registerGuardianNameText, registerGuardianPhoneText, registerHeightText, registerMedicationText,
+            registerNameText, registerPhoneText, registerPreConditionsText, registerPronounsText,
+            registerReligionText, registerWeightText, registerDrugAllergyText, registerOtherAllergyText;
 
     @FXML
     private DatePicker registerBirthDateCalendar, registerCovidBoosterVaccineCalendar, registerCovidPrimaryVaccineCalendar,
             registerCovidSecondaryVaccineCalendar;
 
     @FXML
-    private ChoiceBox<?> registerBloodTypeChoiceBox, registerCovidBoosterVaccineChoice, registerCovidPrimaryVaccineChoice,
+    private ChoiceBox<Object> registerBloodTypeChoiceBox, registerCovidBoosterVaccineChoice, registerCovidPrimaryVaccineChoice,
             registerCovidSecondaryVaccineChoice, registerGenderChoice, registerPregnantChoice, registerRaceChoice,
-            registerSexChoice, registerSexualChoice, registerSubstanceChoice;
+            registerSexChoice, registerSexualChoice, registerSubstanceChoice, registerInsuranceChoice;
+
+    @FXML
+    private CheckBox registerDrugAllergyCheck, registerFoodAllergyCheck, registerInsectAllergyCheck,
+            registerLatexAllergyCheck, registerMoldAllergyCheck, registerPetAllergyCheck, registerInuslinCheck,
+            registerAntibioticsCheck, registerAntiCoagulantCheck, registerIbuprofenCheck, registerNaxCheck;
+
+    @FXML
+    private RadioButton registerHispanicRadio, registerNonHispanicRadio, registerNonReligiousRadio, registerReligiousRadio,
+            registerMaleRadio, registerFemaleRadio, registerSexualYesRadio, registerSexualNoRadio;
 
 
     //INSTANCE VARIABLES FOR VISIT
@@ -55,13 +58,15 @@ public class NurseUserInterfaceController implements Initializable {
     @FXML
     private TabPane tabPane;
 
+    @FXML private Tab registerTab, visitTab, recordsTab, draftsTab;
+
     @FXML
     private CheckBox visitCantDoubleVision, visitAbHurt, visitAcuteCheck, visitArmHurt, visitAsthmaCheck, visitBackHurt,
             visitBronchitisCheck, visitCantBlindness, visitCantBlurredVision, visitCantBreath, visitCantLosingHearing,
             visitCantMoveOne, visitCantPassBowel, visitCantPassUrine, visitCantRemember, visitCantRinging, visitCantSleep,
             visitCantSmell, visitCantSoundsTooLoud, visitCantSpeak, visitCantStopPassingWater, visitCantStopScratch,
             visitCantStopSweat, visitCantSwallow, visitCantTaste, visitCantWalk, visitCantWrite, visitChestHurt,
-            visitChestPainCheck, visitChronicHurt, visitConstipationCheck, visitDizziinessCheck, visitEarHurt,
+            visitChestPainCheck, visitChronicHurt, visitConstipationCheck, visitDizzinessCheck, visitEarHurt,
             visitEpiCheck, visitFeelChills, visitFeelFever, visitFeelLight, visitFeelParesthesia, visitFeelSleepy,
             visitFeverCheck, visitFluFeel, visitGastroCheck, visitHeadHurt, visitHeadInjuryCheck, visitHeadacheText,
             visitIMInjectionCheck, visitLegHurt, visitLowBackCheck, visitMouthFeel, visitNauseaCheck, visitNauseatedFeel,
@@ -79,7 +84,7 @@ public class NurseUserInterfaceController implements Initializable {
     @FXML
     private TextField visitAlbuterolText, visitAspirinText, visitAtropineText, visitBloodPressureText, visitCantOtherText,
             visitDiphenText, visitEpinephrineText, visitFlumaText, visitGlucoseText, visitHydroText, visitIVText,
-            visitLorazepamText, visitMoprhineText, visitNalaxoneText, visitNameText, visitNitroText, visitOtherDiagnosisText,
+            visitLorazepamText, visitMorphineText, visitNalaxoneText, visitNameText, visitNitroText, visitOtherDiagnosisText,
             visitOtherFeel, visitOtherHurtText, visitOtherMedicationText, visitOtherMedicineDosageText, visitRoomNumberText;
 
     @FXML
@@ -90,7 +95,6 @@ public class NurseUserInterfaceController implements Initializable {
 
     @FXML
     private ChoiceBox<?> visitPhysicianChoice;
-
 
 
     @FXML
@@ -118,6 +122,56 @@ public class NurseUserInterfaceController implements Initializable {
 
     }
 
+    @FXML
+    void registerFemaleRadioClicked(ActionEvent event) {
+        System.out.println("femal");
+        if (registerMaleRadio.isArmed()) {
+            System.out.println("male clicked");
+
+            registerMaleRadio.disarm();
+        }
+
+        if (registerMaleRadio.isFocused()) {
+            System.out.println("fkasjld");
+        }
+
+    }
+
+    @FXML
+    void registerHispanicRadioClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerMaleRadioClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerNonHispanicRadioClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerNonReligiousRadioClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerReligiousRadioClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerSexualNoRadioClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registerSexualYesRadioClicked(ActionEvent event) {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         AnchorPane.setTopAnchor(tabPane, 0.0);
@@ -125,6 +179,41 @@ public class NurseUserInterfaceController implements Initializable {
         AnchorPane.setRightAnchor(tabPane, 0.0);
         AnchorPane.setBottomAnchor(tabPane, 0.0);
         overallVBox.fillWidthProperty();
+
+        ObservableList<Object> bloodTypes = FXCollections.observableArrayList();
+        bloodTypes.addAll("A", "B", "AB", "O", "Unknown");
+        registerBloodTypeChoiceBox.setItems(bloodTypes);
+
+        ObservableList<Object> insurances = FXCollections.observableArrayList();
+        insurances.addAll("AETNA", "AFLAC", "American Family Insurance", "American Medical Security",
+                "American National Insurance Company", "Anthem Insurance", "Assurant, Inc.", "Asuris Northwest Health",
+                "BlueCross BlueShield Association", "Celtic Insurance Company", "CIGNA", "College Health IPA",
+                "Connecticare Inc.", "Continental General Insurance Company", "Golden Rule Insurance Company",
+                "Group Health Cooperative", "Group Health Inc.", "Harvard Pilgrim Health Care", "Health Markets",
+                "HUMANA", "Insurance Services of America", "Intermountain Healthcare", "Kaiser Permanente", "LifeWise Health Plan of Arizona",
+                "LifeWise Health Plan of Oregon", "LifeWise Health Plan of Washington", "Medica Minnesota", "Medical Mutual",
+                "Oregon Health Insurance", "Oxford Health Plans, Inc.", "Principal Financial Group, Inc.", "Shelter Insurance",
+                "Unicare Health Insurance", "UnitedHealth Group Inc.", "Vista Health Plan", "Walter Jarvis Insurance Services",
+                "WellPoint", "WPS Health Insurance", "None");
+        registerInsuranceChoice.setItems(insurances);
+
+        ObservableList<Object> covidVaccineTypes = FXCollections.observableArrayList();
+        covidVaccineTypes.addAll("Pfizer-BioNTech", "Moderna ", "Johnson & Johnson’s Janssen", "None ");
+        registerCovidPrimaryVaccineChoice.setItems(covidVaccineTypes);
+        registerCovidSecondaryVaccineChoice.setItems(covidVaccineTypes);
+        registerCovidBoosterVaccineChoice.setItems(covidVaccineTypes);
+
+        ObservableList<Object> substanceList = FXCollections.observableArrayList();
+        substanceList.addAll("Drugs", "Alcohol ", "Smoking", "None");
+        registerSubstanceChoice.setItems(substanceList);
+
+        ObservableList<Object> races = FXCollections.observableArrayList();
+        races.addAll("White", "Black or African American", "American Indian or Alaska Native", "Asian", "Native Hawaiian or Other Pacific Islander");
+        registerRaceChoice.setItems(races);
+
+        ObservableList<Object> genders = FXCollections.observableArrayList();
+        genders.addAll("Male", "Female", "Non-binary", "other");
+        registerGenderChoice.setItems(genders);
 
     }
 }
